bffa503b01469d2bbfb4933d90d08d10f00f3f05
unknown <@.(none)>
2013-01-15 01:07:42 +0600

    trips list modifications

--- TransportSystem/Web/Models/Trips/TripModel.cs
+++ TransportSystem/Web/Models/Trips/TripModel.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace TransportSystem.Area.Web.Models
+namespace TransportSystem.Area.Web.Models.Trips
 {
     public class TripModel
     {
